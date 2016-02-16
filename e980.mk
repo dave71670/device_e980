de1eebfc0b9d365a6fd80c1dac89877202fb7fc4
dave71670 <dave71670@gmail.com>
2016-02-16 16:13:36 -0600

    Name change and edit strings to Carbon vendor

--- e980.mk
+++ e980.mk
@@ -18,12 +18,12 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := full_e980
+PRODUCT_NAME := carbon_e980
 PRODUCT_DEVICE := e980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := Optimus G Pro
